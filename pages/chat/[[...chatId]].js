a7f99e6152480f172e044bb038e2f57455a0840a
Ilie1985 <ilie.marian32@yahoo.com>
2023-05-17 15:05:24 +0100

    create the footer and message form

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,7 +1,16 @@
 import ChatSidebar from "components/chatSidebar/ChatSidebar";
 import Head from "next/head";
+import { useState } from "react";
 
-export default function Home() {
+export default function ChatPage() {
+  const [messageText, setMessageText] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("MESSAGE TEXT", messageText);
+    setMessageText("");
+  };
+
   return (
     <>
       <Head>
@@ -11,7 +20,23 @@ export default function Home() {
         <ChatSidebar />
         <div className="flex flex-col bg-gray-700">
           <div className="flex-1">chat window</div>
-          <footer className="bg-gray-800 p-10">footer</footer>
+          <footer className="bg-gray-800 p-10">
+            <form onSubmit={handleSubmit}>
+              <fieldset className="flex gap-2">
+                <textarea
+                  className="w-full resize-none rounded-md bg-gray-700 p-2 text-white focus:border-emerald-500 focus:bg-gray-600 focus:outline focus:outline-emerald-500 "
+                  placeholder="Send a message..."
+                  value={messageText}
+                  onChange={(e) => {
+                    return setMessageText(e.target.value);
+                  }}
+                />
+                <button type="submit" className="btn">
+                  Send
+                </button>
+              </fieldset>
+            </form>
+          </footer>
         </div>
       </div>
     </>
--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
             {!user && (
               <Link
                 href="/api/auth/login"
-                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600 "
+                className="btn"
               >
                 Login
               </Link>
@@ -35,7 +35,7 @@ export default function Home() {
             {!user && (
               <Link
                 href="/api/auth/signup"
-                className="rounded-md bg-emerald-500 px-4 py-2 text-white hover:bg-emerald-600 "
+                className="btn"
               >
                 Signup
               </Link>
