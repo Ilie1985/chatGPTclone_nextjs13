b3d3e520c05b28c689b09ac5872bebb1c8033f24
Ilie1985 <ilie.marian32@yahoo.com>
2023-05-17 14:33:44 +0100

    Create the ChatSidebar component and add the logout link

--- pages/chat/[[...chatId]].js
+++ pages/chat/[[...chatId]].js
@@ -1,3 +1,4 @@
+import ChatSidebar from "components/chatSidebar/ChatSidebar";
 import Head from "next/head";
 
 export default function Home() {
@@ -7,8 +8,8 @@ export default function Home() {
         <title>New chat</title>
       </Head>
       <div className="grid h-screen grid-cols-[260px_1fr]">
-        <div className="">Sidebar</div>
-        <div className="flex-col flex bg-gray-700">
+        <ChatSidebar />
+        <div className="flex flex-col bg-gray-700">
           <div className="flex-1">chat window</div>
           <footer className="bg-gray-800 p-10">footer</footer>
         </div>
