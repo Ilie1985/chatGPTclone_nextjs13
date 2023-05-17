b11ecd5985c7e917031c864c38df6d0bd646c2c4
Ilie1985 <ilie.marian32@yahoo.com>
2023-05-17 16:00:40 +0100

    set up the OPENAI API point

--- pages/api/chat/sendMessage.js
+++ pages/api/chat/sendMessage.js
@@ -1,3 +1,7 @@
+import { OpenAIEdgeStream } from "openai-edge-stream";
+
+
+
 export const config = {
   runtime: "edge",
 };
@@ -5,6 +9,19 @@ export const config = {
 export default async function handler(req) {
   try {
     const { message } = await req.json();
+    const stream= await OpenAIEdgeStream("https://api.openai.com/v1/chat/completions", {
+      headers:{
+        "content-type":"application/json",
+        Authorization:`Bearer ${process.env.OPENAI_AI_KEY}`,
+      },
+      method:"POST",
+      body:JOSN.stringify({
+        model:"gpt-3.5-turbo",
+        messages:[{content:message, role:"user"}],
+        stream:true,
+      })
+    })
+    return new Resppnse(stream)
   } catch (error) {
     console.log(" AN ERROR OCCURRED IN SEND MESSAGE", error);
   }
